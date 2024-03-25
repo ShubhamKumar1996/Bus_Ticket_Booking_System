177675e928a0be5eb2d0944fe529e43f16cc141c
ShubhamKumar1996 <Nimcetshubhamkumar@gmail.com>
2024-03-25 22:45:48 +0530

    Registration Implementation 03

--- 003_Implementation/db_operations.py
+++ 003_Implementation/db_operations.py
@@ -1,6 +1,26 @@
 from mysql.connector import connection
 
+config = {
+    'user': 'root',
+    'password': 'user',
+    'host': 'localhost',
+    'database': 'register_db',
+    'autocommit': True
+}
+
 def db_connect():
-    conn = connection.MySQLConnection(user='root', password = 'user', host='localhost', database='register_db')
-    conn.autocommit = True
-    return conn
+    try:
+        conn = None
+        conn = connection.MySQLConnection(**config)
+    except  Exception as e:
+        print("Error connecting to MySQL", str(e))
+    finally:
+        return conn
+
+def verify_db_connection(conn):
+    if conn and conn.is_connected():
+        return True
+    return False
+
+def close_db_connection(conn):
+    conn.close()
--- 003_Implementation/main.py
+++ 003_Implementation/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel, Field, EmailStr
-from passlib.context import CryptContext
+from argon2 import PasswordHasher
 import re
 
 import db_operations as db_ops
@@ -9,14 +9,13 @@ class User(BaseModel):
     firstname: str = Field(pattern="[a-zA-Z]{3,10}")
     lastname: str = Field(pattern="[a-zA-Z]{3,10}")
     email: EmailStr
-    mobile: str = Field(pattern="[0-9]{10}")
     password: str = Field(min_length=4, max_length=16)
-
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+    mobile: str = Field(pattern="[0-9]{10}")
 
 app = FastAPI()
 
 def email_exists(cursor, email):
+    '''Returns True if the given email already exists in the database else False'''
     query = '''select exists(select 1 from users where email = %s) as email_found'''
     params = (email, )
     cursor.execute(query, params)
@@ -24,6 +23,7 @@ def email_exists(cursor, email):
     return bool(result)
 
 def mobile_exists(cursor, mobile):
+    '''Returns True if the given mobile already exists in database else False'''
     query = '''select exists(select 1 from users where mobile = %s) as mobile_found'''
     params = (mobile, )
     cursor.execute(query, params)
@@ -31,6 +31,7 @@ def mobile_exists(cursor, mobile):
     return bool(result)
 
 def validate_password(password):
+    '''Verify password must have atleast 1 digit, 1 capital letter, 1 small letter, 1 symbol'''
     if re.search("[0-9]", password) and \
        re.search("[a-z]", password) and \
        re.search("[A-Z]", password) and \
@@ -42,31 +43,14 @@ def validate_password(password):
     else:
         return False
 
-def insert_user_db(cursor, params):
-    query = "INSERT INTO users VALUES(NULL, %s, %s, %s, %s, %s)"
-    cursor.execute(query, params)
-
-
-@app.post("/register", status_code=status.HTTP_200_OK)
+@app.post("/register")
 def register(user: User, response: Response):
-    fname, lname, email, mobile, password = user.__dict__.values()
-    fname, lname, email = fname.lower(), lname.lower(), email.lower()
-    
-    conn = db_ops.db_connect()
-    cursor = conn.cursor()
+    # Unpacking user details
+    fname, lname, email, password, mobile = user.__dict__.values()
 
-    # Check for email existence
-    if email_exists(cursor, email):
-        response.status_code = status.HTTP_409_CONFLICT
-        response.body = response.render("Email address is already registered.")
-        return response
-    
-    # Check for mobile existence
-    if mobile_exists(cursor, mobile):
-        response.status_code = status.HTTP_409_CONFLICT
-        response.body = response.render("Mobile is already registered.")
-        return response
-    
+    # Converting details to lowercase to make below mentioned attributes case insensitive
+    fname, lname, email = fname.lower(), lname.lower(), email.lower()
+
     # Password validation  
     password_validated = validate_password(password)
     if not password_validated:
@@ -74,24 +58,65 @@ def register(user: User, response: Response):
         response.body = response.render('Invalid password format.')
         return response
 
-    # Generate hashed password using input password
-    hashed_password = pwd_context.hash(password)
+    # DB connections
+    conn = db_ops.db_connect()
 
-    # Insert new user in DB
-    insert_user_db(cursor, (fname, lname, email, hashed_password, mobile))
+    # Verify DB Connection
+    if not db_ops.verify_db_connection(conn):
+        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        response.body = response.render("Database connection error.")
+        return response
+    
+    with conn.cursor() as cursor:
+        # Verify email presence in DB
+        if email_exists(cursor, email):
+            response.status_code = status.HTTP_409_CONFLICT
+            response.body = response.render("Email address is already registered.")
+            return response
+    
+        # Verify mobile presence in DB
+        if mobile_exists(cursor, mobile):
+            response.status_code = status.HTTP_409_CONFLICT
+            response.body = response.render("Mobile is already registered.")
+            return response
+
+        # Generate hashed password using input password
+        ph = PasswordHasher()
+        hashed_password = ph.hash(password)
+
+        # Insert new user in DB
+        query = "INSERT INTO users VALUES(NULL, %s, %s, %s, %s, %s)"
+        params = (fname, lname, email, hashed_password, mobile)
+        cursor.execute(query, params)
+        message = f"User with id={cursor.lastrowid} created."
 
     # Close DB connections
-    cursor.close()
-    conn.close()
-
-    return {"message": f"User with id={cursor.lastrowid} created."}
+    db_ops.close_db_connection(conn)
+    return {"message": message}
 
 @app.get("/users")
-def get_users():
+def get_users(response: Response):
     '''Retrieve all user records'''
+    
+    users = []
+
+    # Connect to DB
     conn = db_ops.db_connect()
-    cursor = conn.cursor()
-    query = "SELECT * FROM users order by user_id desc"
-    cursor.execute(query)
-    users = cursor.fetchall()
+    
+    # Verify DB Connection    
+    if not db_ops.verify_db_connection(conn):
+        response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        response.body = response.render("Database connection error.")
+        return response
+    
+    # Fetch existing users from DB
+    with conn.cursor() as cursor:
+        query = "SELECT * FROM users order by user_id desc"
+        cursor.execute(query)
+        users = cursor.fetchall()
+    
+    # Close DB connection
+    db_ops.close_db_connection(conn)
+
+    # Return the list of users
     return  {"users": users}
